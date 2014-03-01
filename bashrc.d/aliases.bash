dddb0d4bbe974a1095ff8d19cb5c206bb4e31537
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-01 15:14:31 +0100

    moved some aliases into misc.bash

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -1,23 +1,16 @@
-# Some useful aliases
-alias dunnet='emacs -batch -l dunnet'
-alias subl=sublime_text
 alias ll='ls -lh'
 alias la='ls -lAh'
 alias lg='la | grep '
 alias findg='find . | grep '
 alias c=clear
-alias gief='sudo apt-get install'
 alias ..='cd ..'
-alias linode='ssh kjetil@kjetilvalle.com'
 alias j='jobs -l'
+alias more='less' # less is more
 alias which='type -a'
 alias path='echo -e ${PATH//:/\\n}'
-alias more='less' # less is more
-alias serve="python -m SimpleHTTPServer"
 
 # add some color to various commands
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
--- bashrc.d/misc.bash
+++ bashrc.d/misc.bash
@@ -6,9 +6,7 @@ export PATH=~/apps/racket/bin:$PATH
 export PATH=~/apps/sbt/bin:$PATH
 export PATH=~/apps/scala-2.10.0/bin:$PATH
 export PATH=~/apps/lein:$PATH
-export PATH=~/apps/rebar:$PATH
 export PATH=~/.cabal/bin:$PATH
-export PATH=~/apps/redis-2.8.3/src:$PATH
 
 # Java stuff
 alias inst='mvn clean install'
@@ -17,6 +15,7 @@ alias kilde='mvn eclipse:eclipse -DdownloadSources=true'
 # SSH aliases
 alias bekklinode='ssh bekk@176.58.126.52'
 alias domjudge='ssh administrator@10.0.30.137'
+alias linode='ssh kjetil@kjetilvalle.com'
 
 # JavaZone
 alias jzold='ssh javabin@176.58.113.146'
@@ -25,3 +24,7 @@ alias jztest='ssh javabin@212.71.238.251'
 alias jztestroot='ssh root@212.71.238.251'
 alias jzprodroot='ssh root@212.71.237.26'
 alias jzstorage='ssh javabin@176.58.125.145'
+
+# Applications
+alias dunnet='emacs -batch -l dunnet'
+alias subl=sublime_text
