c4f4fe208ff142f73bcecae76786eede16793b5a
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-02 21:54:47 +0100

    setting shell color usage

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -8,11 +8,3 @@ alias j='jobs -l'
 alias more='less' # less is more
 alias which='type -a'
 alias path='echo -e ${PATH//:/\\n}'
-
-# add some color to various commands
-if [ -x /usr/bin/dircolors ]; then
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
