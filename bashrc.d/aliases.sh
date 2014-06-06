242e93907de087cddc41386e13eb7bed3a804506
Kjetil Valle <kjetil.valle@gmail.com>
2014-06-06 22:54:15 +0200

    tree aliases

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -8,3 +8,7 @@ alias j='jobs -l'
 alias more='less' # less is more
 alias which='type -a'
 alias path='echo -e ${PATH//:/\\n}'
+
+alias tree2='tree -L 2'
+alias tree3='tree -L 3'
+alias tree4='tree -L 4'
