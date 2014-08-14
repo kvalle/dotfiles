772d518cf45c01f1027506d6389ce91a8501d957
Kjetil Valle <kjetil.valle@gmail.com>
2014-08-14 20:04:29 +0200

    misc

--- bashrc.d/boxen.sh
+++ bashrc.d/boxen.sh
@@ -1,1 +1,3 @@
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
+
+. /opt/boxen/homebrew/etc/bash_completion.d/git-completion.bash
--- bashrc.d/mac-kjetilvalle.sh
+++ bashrc.d/mac-kjetilvalle.sh
@@ -1,1 +1,3 @@
 export PATH=$PATH:/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin
+
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
