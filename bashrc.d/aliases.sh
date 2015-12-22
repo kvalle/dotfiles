d01b91268694b6058e74ebff8d2bd91fa8c4b869
Kjetil Valle <kjetil.valle@vegvesen.no>
2015-12-22 10:12:55 +0100

    cls alias

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -12,3 +12,8 @@ alias path='echo -e ${PATH//:/\\n}'
 alias tree2='tree -L 2'
 alias tree3='tree -L 3'
 alias tree4='tree -L 4'
+
+# alias for "ESC + c"
+#alias cls='echo -en "\33c"' 
+alias cls='echo -en "\ec"'
+#alias cls='clear && echo -en "\e[3J"' # for KDE
