4cae51f43562f82b8601d37555ea911bd45fc065
Kjetil Valle <kjetil.valle@gmail.com>
2016-09-25 11:37:43 +0200

    Updated jz server alias

--- bashrc.d/javabin.sh
+++ bashrc.d/javabin.sh
@@ -3,5 +3,4 @@ alias jzprod='ssh javabin@212.71.237.26'
 alias jztest='ssh javabin@212.71.238.251'
 alias jztestroot='ssh root@212.71.238.251'
 alias jzprodroot='ssh root@212.71.237.26'
-alias jzstorage='ssh javabin@176.58.125.145'
-
+alias jzymse='ssh javabin@176.58.125.145'
