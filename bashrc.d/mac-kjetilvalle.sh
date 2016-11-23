26c65b0e2ec55fde9da8c63899d441913e7d5bd8
Kjetil Valle <kjetil.valle@gmail.com>
2016-11-23 12:53:36 +0100

    Update PATHs for mac-kjetilvalle

--- bashrc.d/mac-kjetilvalle.sh
+++ bashrc.d/mac-kjetilvalle.sh
@@ -1,3 +1,4 @@
-export PATH=$PATH:/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin
+export PATH=$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin
+export PATH=$PATH:/Users/kjetilvalle/Library/Python/2.7/bin
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
