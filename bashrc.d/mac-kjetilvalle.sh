2a8d373fc42059ab70b8999a519192e1fe58bf8c
Kjetil Valle <kjetil.valle@gmail.com>
2017-03-30 11:12:09 +0200

    Remember ssh keys on sierra

--- bashrc.d/mac-kjetilvalle.sh
+++ bashrc.d/mac-kjetilvalle.sh
@@ -2,3 +2,7 @@ export PATH=$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 export PATH=$PATH:/Users/kjetilvalle/Library/Python/2.7/bin
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
+
+# http://apple.stackexchange.com/questions/254468/macos-sierra-doesn-t-seem-to-remember-ssh-keys-between-reboots
+{ eval `ssh-agent`; ssh-add -A; } &>/dev/null
