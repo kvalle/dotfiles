3a227ecfb09875526e575b126317fb1758cf9661
Kjetil Valle <kjetil.valle@gmail.com>
2017-03-30 11:12:44 +0200

    Make pass cache passwords a bit

--- bashrc.d/pass.sh
+++ bashrc.d/pass.sh
@@ -1,2 +1,8 @@
 export PASSWORD_STORE_DIR=/Users/kjetilvalle/prosjekter/nsb/.password-store
 export KEYNAME="$(gpg2 -K | awk 'NR==3 {print $2}' | sed 's/2048R\///g')"
+
+export GPG_TTY=$(tty)
+gpg-connect-agent /bye 2> /dev/null
+if [ $? -ne 0 ]; then
+    eval $(gpg-agent --daemon)
+fi
