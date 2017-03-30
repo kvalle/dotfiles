902f589ff7616f5d777820ee4c364bd6a2e4345a
Kjetil Valle <kjetil.valle@gmail.com>
2017-03-30 18:17:18 +0200

    Set M2_REPO variable

--- bashrc.d/mac-kjetilvalle.sh
+++ bashrc.d/mac-kjetilvalle.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/Users/kjetilvalle/Library/Python/2.7/bin
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
+export M2_REPO="/Users/kjetilvalle/.m2/repository"
 
 # http://apple.stackexchange.com/questions/254468/macos-sierra-doesn-t-seem-to-remember-ssh-keys-between-reboots
 { eval `ssh-agent`; ssh-add -A; } &>/dev/null
