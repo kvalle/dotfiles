9baba24d20e108ef453cfa65dfe1ff0230fbb3d8
Kjetil Valle <kjetil.valle@gmail.com>
2017-11-17 19:20:35 +0100

    Add stuff to PATH

--- bashrc.d/mac-kjetilvalle.sh
+++ bashrc.d/mac-kjetilvalle.sh
@@ -1,5 +1,6 @@
 export PATH=$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 export PATH=$PATH:/Users/kjetilvalle/Library/Python/2.7/bin
+export PATH=$PATH:/Users/kjetilvalle/.local/bin
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
@@ -7,3 +8,6 @@ export M2_REPO="/Users/kjetilvalle/.m2/repository"
 
 # http://apple.stackexchange.com/questions/254468/macos-sierra-doesn-t-seem-to-remember-ssh-keys-between-reboots
 { eval `ssh-agent`; ssh-add -A; } &>/dev/null
+
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
