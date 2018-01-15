3256760ec8907caa9e3c447da51829927969ef44
Kjetil Valle <kjetil.valle@gmail.com>
2018-01-15 12:32:28 +0100

    Update how keyname is extracted from gpg output

--- bashrc.d/pass.sh
+++ bashrc.d/pass.sh
@@ -1,5 +1,5 @@
-export PASSWORD_STORE_DIR=/Users/kjetilvalle/prosjekter/nsb/.password-store
-export KEYNAME="$(gpg2 -K | awk 'NR==3 {print $2}' | sed 's/2048R\///g')"
+export PASSWORD_STORE_DIR=/Users/kjetilvalle/nsb/dev/.password-store
+export KEYNAME="$(gpg -K | awk 'NR==4 {print $1}' | sed 's/2048R\///g')"
 
 export GPG_TTY=$(tty)
 gpg-connect-agent /bye 2> /dev/null
