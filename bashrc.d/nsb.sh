2d0b4679c93369bf1c61cd46713885ca75f23b58
Kjetil Valle <kjetil.valle@gmail.com>
2019-11-13 10:12:16 +0100

    Add backend deploy alias

--- bashrc.d/nsb.sh
+++ bashrc.d/nsb.sh
@@ -4,3 +4,6 @@ export CMS_HOME="/Users/kjetilvalle/prosjekter/nsb/cms-ee-distro-4.7.11/home"
 
 export NSB_SSH_CONFIG_DIR="/Users/kjetilvalle/nsb/dev/infrastructure/ssh_config"
 #export NSB_SCRIPT_DIR="/Users/kjetilvalle/prosjekter/nsb/nsb.no-dev/scripts"
+
+
+alias bd="cd backend && mvn clean install && cd .. && echo | envchain aws ./deploy.sh"
