6a48b67e4531946247f3aa5af3e9763604be9cf9
Kjetil Valle <kjetil.valle@gmail.com>
2026-04-24 09:19:06 +0200

    Cleanup of more old junk

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -3,33 +3,30 @@ set -e
 
 echo "Starting setting up symlinks"
 
-rm ~/.bashrc || true
-ln -s ~/dotfiles/bashrc ~/.bashrc
-
 rm ~/.gitconfig || true
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 rm ~/.gitignore || true
 ln -s ~/dotfiles/gitignore ~/.gitignore
 
-rm ~/.pythonrc || true
-ln -s ~/dotfiles/pythonrc ~/.pythonrc
+rm ~/.pythonrc.py || true
+ln -s ~/dotfiles/pythonrc.py ~/.pythonrc.py
 
 rm ~/.ghci || true
 ln -s ~/dotfiles/ghci ~/.ghci
 
 rm ~/.zprofile || true
-ln -s ~/dotfiles/oh-my-zsh-custom/zprofile ~/.zprofile
+ln -s ~/dotfiles/zprofile ~/.zprofile
 
 rm ~/.zshrc || true
 ln -s ~/dotfiles/zshrc ~/.zshrc
 
-rm ~/.config/starship.toml || true
 mkdir -p ~/.config
+
+rm ~/.config/starship.toml || true
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 
 rm -rf ~/.config/ghostty || true
-mkdir -p ~/.config
 ln -s ~/dotfiles/ghostty ~/.config/ghostty
 
 echo "Done setting up symlinks"
