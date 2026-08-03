7486ea7c46979489a00022a0999d7c1240ea9d3b
Kjetil Valle <kjetil.valle@gmail.com>
2026-08-03 11:34:43 +0200

    Legg til env-variabler for Android-utvikling

--- zsh/environment.sh
+++ zsh/environment.sh
@@ -23,6 +23,12 @@ export PATH="$HOME/.jenv/bin:$PATH"
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # ---------------------------------------------------------------------------
+# Android
+# ---------------------------------------------------------------------------
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH"
+
+# ---------------------------------------------------------------------------
 # Python
 # ---------------------------------------------------------------------------
 export PYTHONSTARTUP=~/.config/python/pythonrc.py
